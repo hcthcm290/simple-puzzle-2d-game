1aaca48de28848f8e7c788d0b4302d104e501909
hcthcm290 <dragonnica123@gmail.com>
2020-06-01 11:47:40 +0700

    Make smooth camera movement

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    private Vector3 velocity = Vector3.zero;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+        Vector3 targetPosition = new Vector3(target.position.x,
+                                        target.position.y,
+                                        transform.position.z
+                                        );
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothing);
+        //transform.LookAt(target);
+    }
+}
