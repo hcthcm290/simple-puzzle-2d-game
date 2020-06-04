9c3e418239798b3a6bafc8c9951c045d7f977df3
hcthcm290 <dragonnica123@gmail.com>
2020-06-04 23:54:52 +0700

    Add a pot that can break when ATKed

---
+++ Assets/Script/Pot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour, Breakable
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Break()
+    {
+        animator.SetBool("ATKed", true);
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
